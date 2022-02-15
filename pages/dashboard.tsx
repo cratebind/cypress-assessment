1c8776afeba117db669eaa016443b853918c349f
Bekah Baker <bekah@cratebind.com>
2022-02-15 14:46:57 -0600

    add side menu for dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,42 @@
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+<div className="min-h-screen flex flex-row bg-gray-100">
+  <div className="flex flex-col w-56 bg-white rounded-r-3xl overflow-hidden">
+    <div className="flex items-center justify-center h-20 shadow-md">
+    <img
+            className="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt="Workflow"
+          />
+    </div>
+    <ul className="flex flex-col py-4">
+      <li>
+        <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-home"></i></span>
+          <span className="text-sm font-medium">Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-user"></i></span>
+          <span className="text-sm font-medium">Profile</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-bell"></i></span>
+          <span className="text-sm font-medium">Notifications</span>
+          <span className="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3 py-px text-red-500">5</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-log-out"></i></span>
+          <span className="text-sm font-medium">Logout</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
   )
 }
